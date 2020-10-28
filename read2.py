f50dbe43e8e791146a76deb30c40ee7827dc3c33
Awad Ahmed <32722982+awadahmed97@users.noreply.github.com>
2020-10-27 22:40:29 -0400

    Updated read 2 looked an there was code that was not supposed to be there

--- read2.py
+++ read2.py
@@ -21,34 +21,6 @@ with open('sample_nodes_1.tsv', 'r') as f:
 	next(f)
 	for line in f:
 		words = line.split()
-		id.append(words[0])
-		kind.append(words[-1])
-	print(id)
-	print(name)
-	print(kind)
-f.close	sources = []
-metaedge = []
-target = []
-with open('sample_edges_1.tsv', 'r') as f:
-	next(f)
-	for line in f:	
-		words = line.split()
-		sources.append(words[0])
-		metaedge.append(words[1])
-		target.append(words[2])	
-	print(sources)
-	print(metaedge)
-	print(target)
-f.close
-id =[]
-name =[]
-kind=[]
-words_counter=2
-compounds = ['Gene','Anatomy','Compound','Disease']
-with open('sample_nodes_1.tsv', 'r') as f:
-	next(f)
-	for line in f:
-		words = line.split()
 		line_len = len(words)
 		i = 2
 		first_word = 1
@@ -66,4 +38,3 @@ with open('sample_nodes_1.tsv', 'r') as f:
 	print(name)
 	print(kind)
 f.close	
-
